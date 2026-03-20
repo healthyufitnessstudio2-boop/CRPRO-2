e2628f6144c15ce411586950563860bd8d291e05
healthyufitnessstudio2-boop <healthyufitnessstudio2@gmail.com>
2026-03-20 15:21:34 +0530

    Update ProductsPage.jsx

--- frontend/src/pages/ProductsPage.jsx
+++ frontend/src/pages/ProductsPage.jsx
@@ -110,7 +110,7 @@ const ProductsPage = () => {
 
       {/* Products Grid */}
       <section className="py-12">
-        <div className=""w-full px-4">
+       <div className="w-full px-4">
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 md:gap-4">
             {filteredProducts.map((product, idx) => (
               <div
